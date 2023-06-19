7d9cc0c4c3faa44dbed4b5ec6b093dc3a58a29c3
raulenzo <raru69@hotmail.com>
2023-06-20 01:01:55 +0200

    movimiento

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
- 
+var triangulo = document.getElementById("triangulo");
+var colorKeyframes = ["red", "pink", "blue", "green", "yellow"];
+function changeTriangleColor() {
+var currentIndex = 0;
+setInterval(function() {
+triangulo.style.backgroundColor = colorKeyframes[currentIndex];
+currentIndex = (currentIndex + 1) % colorKeyframes.length;
+}, 300);
+}
+changeTriangleColor();
+
