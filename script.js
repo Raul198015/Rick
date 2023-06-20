3cdbe4d111b2e52e430943cb858884d2347fa08e
raulenzo <raru69@hotmail.com>
2023-06-20 20:28:04 +0200

    fuera triangulo y cambios de color y forma de cuadrado

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-var triangulo = document.getElementById("triangulo");
+var shape = document.getElementById("shape");
 var colorKeyframes = ["red", "pink", "blue", "green", "yellow"];
-function changeTriangleColor() {
+function changeshapeColor() {
 var currentIndex = 0;
 setInterval(function() {
-triangulo.style.backgroundColor = colorKeyframes[currentIndex];
+shape.style.backgroundColor = colorKeyframes[currentIndex];
 currentIndex = (currentIndex + 1) % colorKeyframes.length;
-}, 300);
+}, 2500);
 }
-changeTriangleColor();
+changeshapeColor();
 
 const urlApi = 'https://rickandmortyapi.com/api/character'
 const config = {
@@ -24,8 +24,4 @@ fetch(urlApi,config)
     nombre.innerHTML = data.results[0].name
     console.log(data.results[0].name)
 })
-
-
-
-
 
