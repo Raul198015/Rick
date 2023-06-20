a14909321527df49afd5e6540deb390f8a53c279
raulenzo <raru69@hotmail.com>
2023-06-20 12:43:06 +0200

    script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var triangulo = document.getElementById("triangulo");
 var colorKeyframes = ["red", "pink", "blue", "green", "yellow"];
 function changeTriangleColor() {
@@ -8,4 +9,23 @@ currentIndex = (currentIndex + 1) % colorKeyframes.length;
 }, 300);
 }
 changeTriangleColor();
+
+const urlApi = 'https://rickandmortyapi.com/api/character'
+const config = {
+    headers: {
+        accept: "application/json",
+    },
+}
+let nombre = document.getElementById('Name')
+
+fetch(urlApi,config)
+.then((respuesta) => respuesta.json())
+.then((data) => {let persons = data.results
+    nombre.innerHTML = data.results[0].name
+    console.log(data.results[0].name)
+})
+
+
+
+
 
