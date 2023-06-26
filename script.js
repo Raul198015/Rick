d60e7b1bb3e087d23d053d6e4ebef9f0f24aca93
ALABERTUS <alabertus@gmail.com>
2023-06-26 10:29:04 +0200

    morty

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ request.onload = function() {
 }
 
 function identityCharacters(jsonObj) {
-    const characters = jsonObj['names'];
-  
+    const characters = jsonObj['results'];
+console.log (characters)    
     for (var i = 0; i < characters.length; i++) {
       const myArticle = document.createElement('article');
       const myH2 = document.createElement('h2');
@@ -27,14 +27,17 @@ function identityCharacters(jsonObj) {
       const myPara3 = document.createElement('p');
       const myPara4 = document.createElement('p');
       const myPara5 = document.createElement('p');
+      const myPara6 = document.createElement('img')
       const myList = document.createElement('ul');
 
       myH2.textContent = characters[i].name;
-      myPara1.textContent = 'status: ' + characters[id].status;
-      myPara2.textContent = 'species: ' + characters[id].specie;
-      myPara3.textContent = 'type:' + characters[id].Type;
-      myPara4.textContent = 'origin:' + characters[id].name, url;
-      myPara5.textContent = 'location:' + characters[id].name, url;
+      myPara1.textContent = 'status: ' + characters[i].status;
+      myPara2.textContent = 'species: ' + characters[i].species;
+      myPara3.textContent = 'type:' + characters[i].Type;
+      myPara4.textContent = 'origin:' + characters[i].name;
+      myPara5.textContent = 'location:' + characters[i].name;
+      myPara6.setAttribute("src", characters[i].image);
+      
   
       myArticle.appendChild(myH2);
       myArticle.appendChild(myPara1);
@@ -42,6 +45,7 @@ function identityCharacters(jsonObj) {
       myArticle.appendChild(myPara3);
       myArticle.appendChild(myPara4);
       myArticle.appendChild(myPara5);
+      myArticle.appendChild(myPara6);
       myArticle.appendChild(myList);
   
       section.appendChild(myArticle);
