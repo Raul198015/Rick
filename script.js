9448e7f8eef5522c8349081403a7ade3cb90893f
ALABERTUS <alabertus@gmail.com>
2023-06-26 10:44:12 +0200

    morty

--- script.js
+++ script.js
@@ -21,31 +21,35 @@ function identityCharacters(jsonObj) {
 console.log (characters)    
     for (var i = 0; i < characters.length; i++) {
       const myArticle = document.createElement('article');
+      const myPara1 = document.createElement('img')
       const myH2 = document.createElement('h2');
-      const myPara1 = document.createElement('p');
       const myPara2 = document.createElement('p');
       const myPara3 = document.createElement('p');
       const myPara4 = document.createElement('p');
       const myPara5 = document.createElement('p');
-      const myPara6 = document.createElement('img')
+      const myPara6 = document.createElement('p');
+      const myPara7 = document.createElement('p');
       const myList = document.createElement('ul');
 
+      myPara1.setAttribute("src", characters[i].image);
       myH2.textContent = characters[i].name;
-      myPara1.textContent = 'status: ' + characters[i].status;
-      myPara2.textContent = 'species: ' + characters[i].species;
-      myPara3.textContent = 'type:' + characters[i].Type;
-      myPara4.textContent = 'origin:' + characters[i].name;
-      myPara5.textContent = 'location:' + characters[i].name;
-      myPara6.setAttribute("src", characters[i].image);
+      myPara2.textContent = 'status: ' + characters[i].status;
+      myPara3.textContent = 'species: ' + characters[i].species;
+      myPara4.textContent = 'type:' + characters[i].type;
+      myPara5.textContent = 'gender:' + characters[i].gender;
+      myPara6.textContent = 'origin:' + characters[i].name;
+      myPara7.textContent = 'location:' + characters[i].name;
+      
       
   
-      myArticle.appendChild(myH2);
       myArticle.appendChild(myPara1);
+      myArticle.appendChild(myH2);
       myArticle.appendChild(myPara2);
       myArticle.appendChild(myPara3);
-      myArticle.appendChild(myPara4);
+      myArticle.appendChild(myPara4)
       myArticle.appendChild(myPara5);
       myArticle.appendChild(myPara6);
+      myArticle.appendChild(myPara7);
       myArticle.appendChild(myList);
   
       section.appendChild(myArticle);
